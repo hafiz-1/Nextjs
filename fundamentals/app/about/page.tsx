f584b386f7c9b7aae2dcf0315909f5b753dd3e06
Umair Arshad <hafizumairarshad63@gmail.com>
2026-08-25 15:41:08 +0500

    styles added to navbar

--- fundamentals/app/about/page.tsx
+++ fundamentals/app/about/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <h1>Home Page</h1>;
+export default function About() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">About Page</h1>
+      <p className="mt-2 text-gray-600">
+        This is our About page.
+      </p>
+    </main>
+  );
 }
